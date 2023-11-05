91d40248473ba493bee19395d7c23c7ee1244e8e
tokiakasu <>
2023-11-05 19:19:06 +0300

    Change way of updating cheatsheets in update-sheets.sh

--- cheatmenu/update-sheets.sh
+++ cheatmenu/update-sheets.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sheets_repo="https://github.com/tokiakasu/cheatmenu-sheets.git"
 sheets_dir="$XDG_CONFIG_HOME/cheatmenu/sheets/"
 
 printf "\n\n[+] Updating cheatsheets\n\n"
 
-if cd "$sheets_dir" && git pull "$sheets_repo";
+if cd "$sheets_dir" && git pull;
 then
     printf "\n[+] Updated!\n"
 else
